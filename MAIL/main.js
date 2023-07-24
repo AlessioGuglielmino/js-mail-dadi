2fcca9e752aa1a82ea17517c342647ccdb11d110
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-24 14:56:08 +0200

    feat: esercizio_completato

--- MAIL/main.js
+++ MAIL/main.js
@@ -7,17 +7,24 @@ const mailAccepted = [
 ];
 // VARIABILE MESSAGGIO ESITO ACCESSO
 
-let accesso = "Spiacenti, alla tua mail non è consentito l'accesso";
-
 // PRENDO LA MAIL INSERITA DALL'UTENTE
-const userMail = prompt("inserisci la tua mail per accedere");
-// const userMail = document.getElementById("user-mail");
-// const sendButton = document.getElementById("send-button");
+// const userMail = prompt("inserisci la tua mail per accedere");
+const inputUserMail = document.getElementById("user-mail");
+const sendButton = document.getElementById("send-button");
+const messaggio = document.getElementById("messaggio");
+
+sendButton.addEventListener("click", function () {
+  const userMail = inputUserMail.value;
+  let accesso = "Spiacenti, alla tua mail non è consentito l'accesso";
+
+  for (let i = 0; i < mailAccepted.length; i++) {
+    if (userMail == mailAccepted[i]) {
+      accesso = "Benvenuto, accesso autorizzato";
+    }
+  }
+  messaggio.innerHTML = accesso;
+});
 
 // INIZIO IL CICLO PER VERIFICARE LA VALIDITA' DELLA MAIL
 
-for (let i = 0; i < mailAccepted.length; i++)
-  if (userMail == mailAccepted[i]) {
-    accesso = "Benvenuto, accesso autorizzato";
-  }
-console.log(accesso);
+// console.log(accesso);
