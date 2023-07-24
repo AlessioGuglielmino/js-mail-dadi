41035a6ef8df9ecebc567ec30346885f326ab131
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-24 16:24:19 +0200

    esercizio-completo

--- DADI/main.js
+++ DADI/main.js
@@ -1,18 +1,30 @@
+// CREO COSTANTI PER IL RANGE MASSIMO DEI NUMERI GENERATI
 const maxRandom = 6;
 const minRandom = 1;
 
-const randomCpu = Math.floor(Math.random() * maxRandom) + minRandom;
-console.log(randomCpu);
-const randomPlayer = Math.floor(Math.random() * maxRandom) + minRandom;
-console.log(randomPlayer);
+// const playerWin = "il player è il vincitore";
+// const cpuWin = "la CPU è il vincitore";
 
-// randomCpu = randomCpu > minRandom;
-// randomCpu = randomCpu < maxRandom;
+const playerNumber = document.getElementById("player-number");
+const cpuNumber = document.getElementById("cpu-number");
+const result = document.getElementById("result");
+const sendButton = document.getElementById("send-button");
 
-if (randomCpu > randomPlayer) {
-  console.log("cpu winner");
-} else if (randomPlayer > randomCpu) {
-  console.log("Player winner");
-} else {
-  console.log("Draw");
-}
+// GENERO NUMERI CASUALI DA 1/6
+sendButton.addEventListener("click", function () {
+  const randomCpu = Math.floor(Math.random() * maxRandom) + minRandom;
+  const randomPlayer = Math.floor(Math.random() * maxRandom) + minRandom;
+
+  playerNumber.innerHTML = "Numero Player: " + randomPlayer;
+  cpuNumber.innerHTML = "Numero CPU: " + randomCpu;
+
+  // IMPOSTO I RISULTATI
+
+  if (randomCpu > randomPlayer) {
+    result.innerHTML = "CPU Winner";
+  } else if (randomPlayer > randomCpu) {
+    result.innerHTML = "Player Winner";
+  } else {
+    result.innerHTML = "Draw";
+  }
+});
